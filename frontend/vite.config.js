371fc33f57c763a914b6ff4617c4b8073b338514
hashirahmed2982 <92557515+hashirahmed2982@users.noreply.github.com>
2024-08-09 18:43:32 +0300

    global variables fixed

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,11 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    https:{
-      key:'./telegram-privateKey.key',
-      cert:'./telegram.crt' ,
-    },
-    host: true,
+    
+    host: 'localhost',
     port: 5173
   }
 })
