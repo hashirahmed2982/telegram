f4d0fa3ef69b371749f81086797ee7d23f4d97c2
hashirahmed2982 <92557515+hashirahmed2982@users.noreply.github.com>
2024-08-11 17:53:26 +0300

    updated

--- server/routes/referalRoutes.js
+++ server/routes/referalRoutes.js
@@ -4,10 +4,12 @@ const refferal = require('../models/refferal');
 const router = express.Router();
 
 router.get('/referalslist/:id', async (req, res) => {
+    console.log("ref",req.params.id)
     try {
         // Find all referrals with the given referrerId
         const referrals = await refferal.find({ referrerId: req.params.id });
         
+    console.log("refa",referrals)
        
 
         res.send(referrals);
